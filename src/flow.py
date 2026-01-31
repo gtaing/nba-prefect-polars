2b3db573a23c3b0ead084b0078cc7662cbd9c09a
gtaing <tainggeorges@gmail.com>
2026-01-31 16:05:47 +0100

    UPDATE: deploy with yaml to managed infra

--- src/flow.py
+++ src/flow.py
@@ -1,5 +1,3 @@
-import os
-
 from prefect import flow
 
 from players.seasons.task import get_player_season_stats
@@ -13,14 +11,3 @@ def season_stats():
 
     print(player_stats_fpath)
     print(team_stats_fpath)
-
-
-if __name__ == "__main__":
-
-    flow.from_source(
-        source="https://github.com/gtaing/nba-prefect-polars.git",
-        entrypoint="src/flow.py:season_stats"
-    ).deploy(
-        name="nba-season-stats-deployment",
-        work_pool_name="my-managed-pool",
-    )
